e023082de844837241a0d48d861aa82ae67488b6
jeneljenel <jwwlee@gmail.com>
2019-10-16 11:46:34 +0200

    added access control to all routes

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ const port = 8333; //CHANGE ON SERVER
 //         }
 //     }
 // }
+
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://me-api.jeneljenel/*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+
 app.use(cors());
 app.use(cookieParser());
 
