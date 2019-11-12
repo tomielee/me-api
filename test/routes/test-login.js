1928a25412f82db59b74393626de69ee185c86a2
jeneljenel <jwwlee@gmail.com>
2019-11-12 17:10:57 +0100

    added database tables.

--- test/routes/test-login.js
+++ test/routes/test-login.js
@@ -12,11 +12,56 @@ const chaiHttp = require('chai-http');
 const server = require('../../app.js');
 const db = require("../../db/database.js");
 
-chai.should(); //use strict
+// Handle hashing password
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
+chai.should(); //use strict
 chai.use(chaiHttp);
 
 describe('Login', () => {
+    /*
+    * Create tables users.
+    */
+    before((done) => {
+        let sql_users = "CREATE TABLE IF NOT EXISTS users (name, email, birthday, password);";
+        db.run(sql_users);
+        done();
+    });
+
+    /*
+    * Insert test content in users.
+    */
+    beforeEach(async function () {
+        const hashed = await bcrypt.hash("Passw0rd!", saltRounds, (err, hash) => { return hash });
+        const body = {
+            name: "Donald Duck",
+            email: "donald.duck@testlogin.com",
+            birthday: "9 June 1934",
+            password: hashed
+        };
+        await db.run("INSERT INTO users (name, email, birthday, password) VALUES(?, ?, ?, ?);", body);
+
+    })
+
+    /*
+    * Delete tables users.
+    */
+    afterEach((done) => {
+        const sql_users = "DELETE FROM users;";
+        db.run(sql_users);
+        done();
+    });
+
+    /*
+    * Drop tables users.
+    */
+    after((done) => {
+        const sql_users = "DROP TABLE IF EXISTS users;";
+        db.run(sql_users);
+        done();
+    });
+
     /*
     * Test the /GET route
     */
@@ -26,7 +71,6 @@ describe('Login', () => {
                 .get("/login")
                 .end((err, res) => {
                     console.log(res.body);
-                    console.log(err);
                     res.should.have.status(200);
                     done();
                 });
@@ -37,25 +81,23 @@ describe('Login', () => {
     * Test the /POST route
     */
     describe('POST /login', () => {
-        it('should get 201 - register a user', (done) => {
-            const body = {
-                name: "Donald Duck",
-                email: "donald.duck@testlogin.com",
-                birthday: "9 June 1934",
-                password: "Passw0rd!"
-            };
+        // it('should get 201 - register a user', (done) => {
+        //     const body = {
+        //         name: "Donald Duck",
+        //         email: "donald.duck@testlogin.com",
+        //         birthday: "9 June 1934",
+        //         password: "Passw0rd!"
+        //     };
 
-            chai.request(server)
-                .post('/register')
-                .send(body)
-                .end((err, res) => {
-                    console.log(res.body);
-                    console.log(err);
-                    res.should.have.status(201);
-                    res.body.should.be.a('object');
-                    done();
-                });
-        });
+        //     chai.request(server)
+        //         .post('/register')
+        //         .send(body)
+        //         .end((err, res) => {
+        //             res.should.have.status(201);
+        //             res.body.should.be.a('object');
+        //             done();
+        //         });
+        // });
 
         it('should get 401 - wrong password.', (done) => {
             const body = {
@@ -91,7 +133,7 @@ describe('Login', () => {
                 });
         });
 
-        it('should get 200 - login success.', (done) => {
+        it('should get 200 - login success.', (done) => {         
             const body = {
                 email: "donald.duck@testlogin.com",
                 password: "Passw0rd!"
@@ -99,8 +141,9 @@ describe('Login', () => {
 
             chai.request(server)
                 .post('/login')
-                .send(body)
+                .send(user)
                 .end((err, res) => {
+                    console.log(err)
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     done();
