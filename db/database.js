3f53f601fc8badec3dad11793bef6678f6dd142d
jeneljenel <jwwlee@gmail.com>
2019-11-14 15:15:43 +0100

    added promisify

--- db/database.js
+++ db/database.js
@@ -1,10 +1,18 @@
-var sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
+const { promisify } = require('util');
 
-module.exports = (
-  function() { 
-    if (process.env.NODE_ENV === 'test') {
-      return new sqlite3.Database('./db/test.sqlite');
-    } 
-    return new sqlite3.Database('./db/texts.sqlite');
-}()
-);
+module.exports = (function() {
+  let db;
+
+  if (process.env.NODE_ENV === 'test') {
+    db = new sqlite3.Database('./db/test.sqlite');
+  } else {
+    db = new sqlite3.Database('./db/texts.sqlite');
+  }
+
+  return {
+    get: promisify(db.get).bind(db),
+    all: promisify(db.all).bind(db),
+    run: promisify(db.run).bind(db),
+  };
+})();
