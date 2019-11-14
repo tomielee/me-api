1d18d7321159a7c0084a7d3d0766438dc5cdfa84
jeneljenel <jwwlee@gmail.com>
2019-11-14 15:17:30 +0100

    clean up. error handler removed from routes. handled by app

--- models/reports.js
+++ models/reports.js
@@ -4,130 +4,33 @@
 */
 "use strict";
 
-
-//Database
-// const sqlite3 = require('sqlite3').verbose();
-// const db = new sqlite3.Database('./db/texts.sqlite');
-const db = require("../db/database.js");
-
-
+// Database
+const db = require("../db/database");
 
 const reports = {
-    addReport: function (res, body) {
-        const title = body.title;
-        const text = body.text;
-        
-        let newReport = [title, text];
+    async add(data) {
+        const { title, text } = data;
+        const sql = "INSERT INTO reports (title, text) VALUES(?, ?)";
 
-        db.run("INSERT INTO reports (title, text) VALUES(?, ?)",
-            newReport, 
-            (err, result) => {
-                if (err) {
-                    return res.status(500).json({
-                        errors: {
-                            source: "/reports",
-                            title: "Database error",
-                            detail: err.message
-                        }
-                    });
-                }
-
-                return res.status(201).json({
-                    data: {
-                        msg: "Report sucessfully added!"
-                    }
-                });
-
-            }
-        );
+        return db.run(sql, [title, text]);
     },
 
-    editReport: function (res, body) {
-        const id = body.id;
-        const title = body.title;
-        const text = body.text;
+    async edit(data) {
+        const { id, title, text } = data;
+        const sql = "UPDATE 'reports' SET title = ?, text = ? WHERE id = ?;";
 
-        let newReport = [title, text, id];
-        // console.log(newReport);
-
-        let query = "UPDATE reports SET title='" + title + "', text='" + text + "' WHERE id=" + id + ";";
-            // console.log(query);
-        db.run(query,
-            (err, result) => {
-                if (err) {
-                    return res.status(500).json({
-                        errors: {
-                            source: "/reports/edit",
-                            title: "Database error",
-                            detail: err.message
-                        }
-                    });
-                }
-
-                return res.status(201).json({
-                    data: {
-                        msg: "Report sucessfully updated!"
-                    }
-                });
-
-            }
-        );
+        return db.run(sql, [title, text, id]);
     },
 
-    getAllReports: function (res) {
-        const sql = "SELECT * FROM reports;";
-        
-        db.all(
-            sql,
-            (err, result) => {
-                if (err) {
-                    return res.status(500).json({
-                        errors: {
-                            source: "/reports",
-                            title: "Database error",
-                            detail: err.message
-                        }
-                    });
-                }
-                
-                // console.log("skickar med" , result);
-
-                return res.json(result);
-            } 
-        )
-        
-
+    async all() {
+        return db.all("SELECT * FROM reports;");
     },
 
-    getReport: function (res, params) {
-        const sql = "SELECT * FROM reports WHERE id IS(?);";
-        const id = params.id;
+    async get(id) {
+        const sql = "SELECT * FROM reports WHERE id = ?;";
 
-        db.get(
-            sql,
-            id,
-            (err, result) => {
-                if (err) {
-                    return res.status(500).json({
-                        errors: {
-                            source: "/reports/week/:id",
-                            title: "Database error",
-                            detail: err.message
-                        }
-                    });
-                }
-
-                let report = result;
-
-                return res.json({
-                    title: report.title,
-                    text: report.text,
-                    id: report.id
-                });
-            }
-        );
-    }
+        return db.get(sql, [id]);
+    },
 }
-
 
 module.exports = reports;
